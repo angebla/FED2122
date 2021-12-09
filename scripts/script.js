5263f5f0ee17fb2f887b9a83a27ed9d87204864b
angebla <47508364+angebla@users.noreply.github.com>
2021-12-10 00:26:42 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
-// JavaScript Document
+var knop = document.querySelector("header svg");
+var kruis = document.querySelector("nav > img");
+var nav = document.querySelector("nav");
+
+function slide(){
+	nav.classList.add("navigatie");
+}
+
+knop.addEventListener("click", slide);
+
+
+
+function unslide(){
+	nav.classList.remove("navigatie");
+}
+
+kruis.addEventListener("click", unslide)
